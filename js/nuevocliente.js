eab2b56614964b0f130301ba696d4a8d5ba49abe
Mati-favre <75177286+underling131121@users.noreply.github.com>
2022-05-29 19:05:28 +0300

    CRMCRUDINDEX

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -2,11 +2,22 @@
 (function(){
     let DB;
     const formulario = document.querySelector('#formulario');
-    document.addEventListener('DOMContentLoaded', ()=>{
+    document.addEventListener('DOMContentLoaded', () =>{
         // conectar a la DB
         conectarDB();
         formulario.addEventListener('submit',validarCliente);
     });
+
+    function conectarDB(){
+        // Window.indexedDB crea una DB y si ya existe se conecta
+        const abrirConexion = window.indexedDB.open('crm',1);
+        abrirConexion.error = function(){
+            console.log('Error')
+        };
+        abrirConexion.onsuccess = function(){
+            DB = abrirConexion.result; 
+        }
+    }
 
     function validarCliente(e){
     e.preventDefault();
@@ -15,6 +26,7 @@
     const email = document.querySelector('#email').value;
     const telefono = document.querySelector('#telefono').value;
     const empresa = document.querySelector('#empresa').value;
+
     if(nombre === '' || email === '' || telefono === '' || empresa === ''){
         imprimirAlerta('Todos los campos son obligatorios','error');
             return;
@@ -29,12 +41,15 @@
         cliente.id = Date.now();
         crearNuevoCliente(cliente);
     }
+
     // pasar el object literal enhancement como solo parametro
     function crearNuevoCliente(cliente){
-        const transaction = DB.transaction(['crm'],'readwrite');
-        const objectStore = transaction.objectStore('crm');
+
+            const transaction = DB.transaction(['crm'],'readwrite');
+            const objectStore = transaction.objectStore('crm');
+
         // agregar nuevo registro de cliente
-        objectStore.add(cliente)
+        objectStore.add(cliente);
         // Mensaje de error
         transaction.onerror = function(){
             imprimirAlerta('Hubo un error','error');
